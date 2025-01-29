7679ef573d56e148233dc3d956bc6ac0e6d53d5d
Kyle Addis <kyle.a.addis@gmail.com>
2025-01-29 21:36:19 +0530

    hud finished

--- main.gd
+++ main.gd
@@ -14,11 +14,17 @@ func _process(delta: float) -> void:
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	get_tree().call_group("mobs", "queue_free")
+
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
@@ -48,6 +54,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
